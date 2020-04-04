9bc720e380418e5f12332735e3d981836fa9b401
karol-wolski <poczta@karol-wolski.pl>
2020-04-04 11:22:55 +0000

    Deploying to gh-pages from  @ 774ce19786645d977e008f1c39a9b9eb90209a8f 🚀

---
+++ precache-manifest.d1720e7b2c295ab63d97390d1d8ff1ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3633ef45e5de97efaa0fb90a0b233bae",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "643b933708b0192b1214",
+    "url": "/covid-19-stats/static/js/2.5e069626.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-stats/static/js/2.5e069626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e740cefb2d556bda4535",
+    "url": "/covid-19-stats/static/js/main.a0ad6478.chunk.js"
+  },
+  {
+    "revision": "a97935a10b12fd7e2ebc",
+    "url": "/covid-19-stats/static/js/runtime-main.e812401a.js"
+  }
+]);
