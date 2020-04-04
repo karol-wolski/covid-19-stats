81247dac1f9b794237f339c9d0451e33bbb9d972
karol-wolski <poczta@karol-wolski.pl>
2020-04-04 12:03:22 +0000

    Deploying to gh-pages from  @ a2e7ac57078dc927a6dcc12cb072f8b4fa547521 🚀

---
+++ precache-manifest.b198fbdfbfc35beca0a86f72ed877d1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcd941afa413a90ce25c8409f6a33f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eda7ddd2ca51a23b5d1e",
+    "url": "/static/js/2.390e1622.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.390e1622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e740cefb2d556bda4535",
+    "url": "/static/js/main.a0ad6478.chunk.js"
+  },
+  {
+    "revision": "496a5239064921291408",
+    "url": "/static/js/runtime-main.1a95108c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-stats/precache-manifest.d1720e7b2c295ab63d97390d1d8ff1ff.js"
+  "/precache-manifest.b198fbdfbfc35beca0a86f72ed877d1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-stats/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
